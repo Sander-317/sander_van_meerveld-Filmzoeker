abe71e537d7704e8f64114ba18e3630a96a83e7b
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-08 13:54:43 +0200

    Added function to show all movie titles

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 
 console.log("test")
 console.log(movies)
+
+const movieList = document.getElementById("movie-list")
+
+const movieTitles = movies.map((item) => {
+    movieTitle = item.Title
+    let newLi = document.createElement("li")
+    newLi.innerHTML = `${movieTitle}`
+    movieList.appendChild(newLi)
+
+})
+
+console.log(movieTitles)
+// movieList.appendChild(movieTitles)
+// addMoviesToDom(movieTitles)
+
+
+
+
+
+
+// function addMoviesToDom(movieArray) {
+//         let newLi = movieArray.forEach(item => document.createElement("li"))
+//         // newLi.innerHTML = `${item}`
+//         // movieList.appendChild(newLi)
+        
+//         console.log(typeof newLi)
+//         return newLi
+// }
+

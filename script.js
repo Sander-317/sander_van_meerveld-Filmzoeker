eff8cbfa070e901287fff59e71e11f8abc6d8939
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-08 14:13:49 +0200

    added function addMoviesToDom

--- script.js
+++ script.js
@@ -5,28 +5,22 @@ console.log(movies)
 const movieList = document.getElementById("movie-list")
 
 const movieTitles = movies.map((item) => {
-    movieTitle = item.Title
-    let newLi = document.createElement("li")
-    newLi.innerHTML = `${movieTitle}`
-    movieList.appendChild(newLi)
+    moviePoster = item.Poster
+    let newPoster = document.createElement("img")
+    newPoster.src = `${moviePoster}`
+    // movieList.appendChild(newPoster)
+    addMoviesToDom(newPoster)
 
 })
 
-console.log(movieTitles)
-// movieList.appendChild(movieTitles)
-// addMoviesToDom(movieTitles)
+function addMoviesToDom(movieArray){
+    movieList.appendChild(movieArray)
+}
 
+// const movieTitles = movies.map((item) => {
+//     movieTitle = item.Title
+//     let newLi = document.createElement("li")
+//     newLi.innerHTML = `${movieTitle}`
+//     movieList.appendChild(newLi)
 
-
-
-
-
-// function addMoviesToDom(movieArray) {
-//         let newLi = movieArray.forEach(item => document.createElement("li"))
-//         // newLi.innerHTML = `${item}`
-//         // movieList.appendChild(newLi)
-        
-//         console.log(typeof newLi)
-//         return newLi
-// }
-
+// })

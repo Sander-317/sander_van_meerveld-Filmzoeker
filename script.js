65abc234aa9bdd01e7d82b3f7e042ea52ea8b819
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-07-02 13:23:28 +0200

    added feed back

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 
-console.log("test")
-console.log(movies.length)
+// console.log("test")
+// console.log(movies.length)
 
 const movieList = document.getElementById("movie-list")
 document.querySelectorAll(".radio-button").forEach(button => button.addEventListener("change", handleOnChangeEvent ))
-movieList.innerHTML =""
+// movieList.innerHTML =""
 addMoviesToDom(movies)
 
 function handleOnChangeEvent(event){
-    console.log(event.target)
+    // console.log(event.target)
     switch(event.target.value) {
         case "new-movies":
-            console.log("new-movies")
+            // console.log("new-movies")
             filterLatestMovies()
             break
         case "avengers":
-            console.log("avengers")
+            // console.log("avengers")
             filterMovies("Avengers")
             break
         case "x-men":
-            console.log("x-men")
+            // console.log("x-men")
             filterMovies("X-Men")
             break
         case "princess":
-            console.log("princess")
+            // console.log("princess")
             filterMovies("Princess")
             break
         case "batman":
-            console.log("batman")
+            // console.log("batman")
             filterMovies("Batman")
             break
         default:
@@ -42,7 +42,7 @@ function filterMovies(filter){
     filteredMovies = movies.filter((item) => {
         return item.Title.includes(filter) 
         })
-    console.log(filteredMovies)
+    // console.log(filteredMovies)
     addMoviesToDom(filteredMovies)
 }
 
@@ -50,7 +50,7 @@ function filterLatestMovies() {
     movieList.innerHTML =""
     filteredMovies = []
     filteredMovies = movies.filter((item) =>  item.Year >= "2014")
-    console.log(filteredMovies)
+    // console.log(filteredMovies)
     addMoviesToDom(filteredMovies)
 
 }
@@ -60,8 +60,8 @@ function addMoviesToDom(movieArray){
         let newLiTag = document.createElement("li")
         let newATag = document.createElement("a")
         let newPoster = document.createElement("img")
-        moviePoster = item.Poster
-        let imdbId = item.imdbID
+        let moviePoster = item.Poster
+        const imdbId = item.imdbID
         newATag.href = makeImdbLink(imdbId)
         newATag.target = "_blank"
        newATag.appendChild(newPoster)
